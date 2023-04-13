0a2f874dec7d2d56eacba0478ed030a564f8246d
Juhniel <juhn.sweden@gmail.com>
2023-04-13 13:45:22 +0200

    highscore changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+
 
 export default function App() {
-  const [nameInputValue, setNameInputValue] = useState('');
-  const [submittedName, setSubmittedName] = useState('');
+  const [nameInputValue, setNameInputValue] = useState("");
+  const [submittedName, setSubmittedName] = useState("");
+  
   const choices = ["rock", "paper", "scissors"];
 
   function handleInputChange(e) {
@@ -13,7 +15,6 @@ export default function App() {
     e.preventDefault();
     setSubmittedName(nameInputValue);
     console.log(nameInputValue); // Logging the name
-
   }
 
   function handleRPSChoice(e) {
@@ -22,29 +23,44 @@ export default function App() {
 
   function handleComputerChoice() {
     let computerChoice;
-    const computerChoiceNumber = Math.floor(Math.random()*3)
+    const computerChoiceNumber = Math.floor(Math.random() * 3);
     computerChoice = choices[computerChoiceNumber];
     return computerChoice;
   }
 
   function gameResult(playerChoice, computerChoice) {
-
     console.log(playerChoice);
     console.log(computerChoice);
 
     if (playerChoice === computerChoice) {
-      console.log("Tie!"); 
-    } else if(
+      console.log("Tie!");
+    } else if (
       (playerChoice === "rock" && computerChoice === "scissors") ||
       (playerChoice === "scissors" && computerChoice === "paper") ||
       (playerChoice === "paper" && computerChoice === "rock")
-    ){
+    ) {
       console.log("Player wins!");
-    }else {
-      console.log("Computer wins!")
+    } else {
+      console.log("Computer wins!");
     }
   }
-  
+
+  const [highScores, setHighScores] = useState([]);
+
+  useEffect(() => {
+    async function getHighscore() {
+      const url =
+        'https://highscore-react-default-rtdb.europe-west1.firebasedatabase.app/highscores.json';
+      const response = await fetch(url);
+      const data = await response.json();
+      const highScore = Object.entries(data);
+
+      highScore.sort((a, b) => b[1].score - a[1].score);
+      setHighScores(highScore);
+    }
+
+    getHighscore();
+  }, []);
 
   return (
     <>
@@ -65,13 +81,33 @@ export default function App() {
       {submittedName && <p>Player: {submittedName}</p>}
 
       <div className="RPSContainer">
-        <button className="RPSButtons" value="rock" onClick={handleRPSChoice}>Rock</button>
-        <button className="RPSButtons" value="paper" onClick={handleRPSChoice}>Paper</button>
-        <button className="RPSButtons" value="scissors" onClick={handleRPSChoice}>Scissor</button>
+        <button className="RPSButtons" value="rock" onClick={handleRPSChoice}>
+          Rock
+        </button>
+        <button className="RPSButtons" value="paper" onClick={handleRPSChoice}>
+          Paper
+        </button>
+        <button
+          className="RPSButtons"
+          value="scissors"
+          onClick={handleRPSChoice}
+        >
+          Scissor
+        </button>
       </div>
 
       <h1>Highscore</h1>
-      <div id="score-list"></div>
+      <div id="score-list">
+        <ol>
+          {highScores.map(([key, userObj]) => (
+            <li key={key}>
+              {userObj.name} - Score: {userObj.score}
+            </li>
+          ))}
+        </ol>
+      </div>
     </>
   );
 }
+
+
