5355cb4331350b81a692d0b73ba041845123c5a5
Juhniel <juhn.sweden@gmail.com>
2023-04-13 12:18:22 +0200

    Functional logic for rock scissor paper

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,77 @@
+import React, { useState } from 'react';
+
 export default function App() {
+  const [nameInputValue, setNameInputValue] = useState('');
+  const [submittedName, setSubmittedName] = useState('');
+  const choices = ["rock", "paper", "scissors"];
+
+  function handleInputChange(e) {
+    setNameInputValue(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setSubmittedName(nameInputValue);
+    console.log(nameInputValue); // Logging the name
+
+  }
+
+  function handleRPSChoice(e) {
+    gameResult(e.target.value, handleComputerChoice());
+  }
+
+  function handleComputerChoice() {
+    let computerChoice;
+    const computerChoiceNumber = Math.floor(Math.random()*3)
+    computerChoice = choices[computerChoiceNumber];
+    return computerChoice;
+  }
+
+  function gameResult(playerChoice, computerChoice) {
+
+    console.log(playerChoice);
+    console.log(computerChoice);
+
+    if (playerChoice === computerChoice) {
+      console.log("Tie!"); 
+    } else if(
+      (playerChoice === "rock" && computerChoice === "scissors") ||
+      (playerChoice === "scissors" && computerChoice === "paper") ||
+      (playerChoice === "paper" && computerChoice === "rock")
+    ){
+      console.log("Player wins!");
+    }else {
+      console.log("Computer wins!")
+    }
+  }
+  
+
   return (
     <>
-    <h1 className="header">Rock, Scissor, Paper</h1>
-      <form className="new-player-form">
+      <h1 className="header">Rock, Scissor, Paper</h1>
+      <form onSubmit={handleSubmit} className="new-player-form">
         <div>
           <label htmlFor="nameInput">Enter name:</label>
-          <input type="text" id="nameInput"/>
+          <input
+            type="text"
+            id="nameInput"
+            value={nameInputValue}
+            onChange={handleInputChange}
+          />
         </div>
-        <button className="btn"></button>
+        <button className="nameButton">Submit</button>
       </form>
+
+      {submittedName && <p>Player: {submittedName}</p>}
+
+      <div className="RPSContainer">
+        <button className="RPSButtons" value="rock" onClick={handleRPSChoice}>Rock</button>
+        <button className="RPSButtons" value="paper" onClick={handleRPSChoice}>Paper</button>
+        <button className="RPSButtons" value="scissors" onClick={handleRPSChoice}>Scissor</button>
+      </div>
+
       <h1>Highscore</h1>
-      <div></div>
+      <div id="score-list"></div>
     </>
-  )
+  );
 }
