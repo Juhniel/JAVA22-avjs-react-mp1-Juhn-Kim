1060507db111bcd98931aedf3e3ff43230b93d75
Juhniel <juhn.sweden@gmail.com>
2023-04-17 15:16:11 +0200

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import React, { useState, useEffect } from "react";
+import PlayAgain from "./PlayAgain";
 
 
 export default function App() {
   const [nameInputValue, setNameInputValue] = useState("");
   const [submittedName, setSubmittedName] = useState("");
-  
+  const [computerOption, setComputerOption] = useState("");
+  const [playerOption, setPlayerOption] = useState("");
+  const [gameWinner, setGameWinner] = useState("");
+
+  const [playerScore, setPlayerScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
+
+  const [highScores, setHighScores] = useState([]);
+  const [gameOverStatus, setGameOverStatus] = useState(false);
+
   const choices = ["rock", "paper", "scissors"];
+
+
+  useEffect(() => {
+    if (computerScore === 1) {
+      gameOver(playerScore, submittedName);
+    }
+  }, [computerScore]);
+
 
   function handleInputChange(e) {
     setNameInputValue(e.target.value);
@@ -13,8 +31,8 @@ export default function App() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    setSubmittedName(nameInputValue);
-    console.log(nameInputValue); // Logging the name
+    setSubmittedName(nameInputValue || "Player");
+    console.log(nameInputValue, submittedName); // Logging the name
   }
 
   function handleRPSChoice(e) {
@@ -29,38 +47,107 @@ export default function App() {
   }
 
   function gameResult(playerChoice, computerChoice) {
+    setPlayerOption(playerChoice);
+    setComputerOption(computerChoice);
     console.log(playerChoice);
     console.log(computerChoice);
 
     if (playerChoice === computerChoice) {
       console.log("Tie!");
+      setGameWinner("Tie");
     } else if (
       (playerChoice === "rock" && computerChoice === "scissors") ||
       (playerChoice === "scissors" && computerChoice === "paper") ||
       (playerChoice === "paper" && computerChoice === "rock")
     ) {
       console.log("Player wins!");
+      setPlayerScore(playerScore + 1);
+      setGameWinner("Player wins!");
     } else {
       console.log("Computer wins!");
+      setComputerScore(computerScore + 1);
+      setGameWinner("Computer wins!");
     }
   }
 
-  const [highScores, setHighScores] = useState([]);
+  function handlePlayAgain() {
+    setGameOverStatus(false);
+    setGameWinner("");
+    setPlayerOption("");
+    setComputerOption("");
+  }
 
-  useEffect(() => {
-    async function getHighscore() {
-      const url =
-        'https://highscore-react-default-rtdb.europe-west1.firebasedatabase.app/highscores.json';
-      const response = await fetch(url);
-      const data = await response.json();
-      const highScore = Object.entries(data);
+  async function gameOver(playerScore, submittedName) {
+    setGameOverStatus(true);
+    setPlayerScore(0);
+    setComputerScore(0);
 
-      highScore.sort((a, b) => b[1].score - a[1].score);
-      setHighScores(highScore);
+    await addHighscore(playerScore, submittedName);
+  }
+
+  async function addHighscore(playerScore, submittedName) {
+    //   event.preventDefault();
+    const url =
+      "https://highscore-react-default-rtdb.europe-west1.firebasedatabase.app/highscores.json";
+    const response = await fetch(url);
+    const data = await response.json();
+    const highScore = Object.entries(data);
+
+    // Find the lowest score and its key
+    let lowestScore = Infinity;
+    let lowestScoreKey = null;
+
+    for (const [key, userObj] of highScore) {
+      if (userObj.score < lowestScore) {
+        lowestScore = userObj.score;
+        lowestScoreKey = key;
+      }
     }
 
+    // Replace the lowest score if the new score is higher
+    if (playerScore > lowestScore) {
+      console.log("The new score is higher than the lowest score");
+      await patchFunction(submittedName, playerScore, lowestScoreKey);
+      await getHighscore();
+    } else {
+      console.log("The new score is not high enough to enter the leaderboard");
+    }
+  }
+
+  async function patchFunction(playerName, playerScore, key) {
+    const newURL = `https://highscore-react-default-rtdb.europe-west1.firebasedatabase.app/highscores/${key}.json`;
+
+    const newHighscore = {
+      name: playerName,
+      score: playerScore,
+    };
+
+    const options = {
+      method: "PATCH",
+      body: JSON.stringify(newHighscore),
+      Headers: {
+        "Content-type": "application/json",
+      },
+    };
+
+    const newResponse = await fetch(newURL, options);
+    await newResponse.json();
+  }
+
+  useEffect(() => {
     getHighscore();
   }, []);
+
+  async function getHighscore() {
+    const url =
+      "https://highscore-react-default-rtdb.europe-west1.firebasedatabase.app/highscores.json";
+    const response = await fetch(url);
+    const data = await response.json();
+    const highScore = Object.entries(data);
+
+    highScore.sort((a, b) => b[1].score - a[1].score);
+    setHighScores(highScore);
+  }
 
   return (
     <>
@@ -78,7 +165,21 @@ export default function App() {
         <button className="nameButton">Submit</button>
       </form>
 
-      {submittedName && <p>Player: {submittedName}</p>}
+      {submittedName && <p>Welcome {submittedName}!</p>}
+      {playerOption && (
+        <p>
+          {submittedName ? submittedName : "Player"} - chose: {playerOption}
+        </p>
+      )}
+      {computerOption && <p>Computer chose: {computerOption}</p>}
+      {gameWinner && <p>Winner of the game is: {gameWinner}</p>}
+      {gameWinner && (
+        <p>
+          {playerScore} - {computerScore}
+        </p>
+      )}
+
+      
 
       <div className="RPSContainer">
         <button className="RPSButtons" value="rock" onClick={handleRPSChoice}>
@@ -96,6 +197,8 @@ export default function App() {
         </button>
       </div>
 
+      {gameOverStatus && <PlayAgain onPlayAgain={handlePlayAgain} />}
+
       <h1>Highscore</h1>
       <div id="score-list">
         <ol>
@@ -109,5 +212,3 @@ export default function App() {
     </>
   );
 }
-
-
---
+++ src/PlayAgain.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./PlayAgain.css";
+
+export default function PlayAgain(props) {
+    function handlePlayAgain(e) {
+      e.preventDefault();
+      props.onPlayAgain();
+    }
+  
+    return (
+      <div className="playAgainContainer">
+        <h1>Game Over!</h1>
+        <p>Do you want to play again?</p>
+        <button className="playAgainButton" onClick={handlePlayAgain}>
+          Play Again
+        </button>
+      </div>
+    );
+  }
